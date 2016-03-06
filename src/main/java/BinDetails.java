5402bf8b893da0f6a0f5a03351fe789bf047092a
kovbh01 <kovbh01@ca.com>
2016-03-06 11:51:57 +0530

    Initial commit

---
+++ src/main/java/BinDetails.java
@@ -0,0 +1,16 @@
+/**
+ * Created by kovbh01 on 3/6/2016.
+ */
+public class BinDetails {
+
+    private final String latitude;
+    private final String longitude;
+    private final String percent;
+
+
+    public BinDetails(String latitude, String longitude, String percent) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.percent = percent;
+    }
+}
